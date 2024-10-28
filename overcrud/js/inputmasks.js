506d047f8c0abf6a1dd60d9ea0e55ee75a9ff799
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-10-28 12:28:49 -0300

    Máscara para CPF

--- overcrud/js/inputmasks.js
+++ overcrud/js/inputmasks.js
@@ -1,7 +1,8 @@
+//------------------------------------------------------------------
 //MÁSCARA PARA TELEFONE
 const handlePhone = (evento) => {
-    let input = evento.target;
-    input.value = phoneMask(input.value);
+    let inputPhone = evento.target;
+    inputPhone.value = phoneMask(inputPhone.value);
 };
 
 const phoneMask = (valor) => {
@@ -11,4 +12,20 @@ const phoneMask = (valor) => {
     valor = valor.replace(/(\d)(\d{4})$/,"$1-$2");
     return valor;
 };
+
+
+//------------------------------------------------------------------
+//MÁSCARA PARA CPF
+const handleCpf = (evento) => {
+    let inputCpf = evento.target;
+    inputCpf.value = cpfMask(inputCpf.value);
+};
+
+function cpfMask(valor){
+    valor = valor.replace(/\D/g,"");
+    valor = valor.replace(/(\d{3})(\d)/,"$1.$2")   ;
+    valor = valor.replace(/(\d{3})(\d)/,"$1.$2");
+    valor = valor.replace(/(\d{3})(\d{1,2})$/,"$1-$2");
+    return valor;
+}
 
