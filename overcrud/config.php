ebfa5aa65a545b92d5ffbbab501d1452755d7876
Kaike A. Maróstica <46039151+kaikem@users.noreply.github.com>
2024-10-28 14:20:33 -0300

    Update config.php
    Adicionado o servidor de casa para testes.

--- overcrud/config.php
+++ overcrud/config.php
@@ -1,7 +1,18 @@
 <?php
+/*BD SHANGAI ESCRITÓRIO
 $db_name = 'phppdo';
 $db_host = 'localhost:3307';
 $db_user = 'root';
 $db_password = 'admin123';
 
 $pdo = new PDO("mysql:dbname=$db_name;host=$db_host", $db_user, $db_password);
+*/
+
+//BD CASA
+$db_name = 'phppdo';
+$db_host = 'localhost:3306';
+$db_user = 'root';
+$db_password = '';
+
+$pdo = new PDO("mysql:dbname=$db_name;host=$db_host", $db_user, $db_password);
+?>
