b98e482e7b15a5260d77b5ec29866669268f6bdd
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-10-29 11:27:36 -0300

    Create support.php
    Métodos auxiliares.

---
+++ overcrud/support.php
@@ -0,0 +1,12 @@
+<?php
+//----------------------------------------------------------------------
+//FUNÇÕES DE SUPORTE
+//mensagem
+function mensagemRetorno($texto, $tipo)
+{
+    echo "<div class='alert alert-$tipo'>$texto</div>";
+}
+
+//function BotaoVoltar($pagina){
+//    echo "<a href='$pagina' class='btn btn-warning'>VOLTAR</a>";
+//}
