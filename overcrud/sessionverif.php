d2cbdcbd419693559314ff4fb4c550cf97816062
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-10-31 15:30:16 -0300

    Create sessionverif.php
    Verificação de sessão ativa em cada página da aplicaão.

---
+++ overcrud/sessionverif.php
@@ -0,0 +1,12 @@
+<?php
+//INÍCIO DE SESSÃO
+session_start();
+
+//VERIFICAÇÃO DE SESSÃO EXISTENTE
+if ((isset($_SESSION['login']) == true) && (isset($_SESSION['senha']) == true)) {
+    $logado = $_SESSION['login'];
+} else {
+    unset($_SESSION['login']);
+    unset($_SESSION['senha']);
+    header("Location: index.php");
+};
