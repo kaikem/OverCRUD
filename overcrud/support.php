75cd0a0b53ee1baa5cc425b1ba116c3eb71008eb
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-10-31 10:37:19 -0300

    Update support.php

--- overcrud/support.php
+++ overcrud/support.php
@@ -7,6 +7,7 @@ function mensagemRetorno($texto, $tipo)
     echo "<div class='alert alert-$tipo'>$texto</div>";
 }
 
+
 //function BotaoVoltar($pagina){
 //    echo "<a href='$pagina' class='btn btn-warning'>VOLTAR</a>";
 //}
