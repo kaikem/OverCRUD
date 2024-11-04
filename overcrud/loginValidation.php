7208fda1cbce98b8968a7347f01c0da59fda5ca7
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-11-04 11:23:58 -0300

    Update loginValidation.php
    Pegar o tipo de usuário e mensgem de erro de login.

--- overcrud/loginValidation.php
+++ overcrud/loginValidation.php
@@ -7,17 +7,24 @@ if (!empty($_POST['login']) && !empty($_POST['senha'])) {
 
     $loginIndex = $_POST['login'];
     $senhaIndex = $_POST['senha'];
+    $usuarioIndex = [];
 
     $sqlVerifUsu = $pdo->query("SELECT * FROM usuarios WHERE `login`='$loginIndex' AND `password`='$senhaIndex'");
 
     if ($sqlVerifUsu->rowCount() > 0) {
+        $usuarioIndex = $sqlVerifUsu->fetchAll(PDO::FETCH_ASSOC);
+        $tipoIndex = $usuarioIndex[0]['tipo'];
+
         $_SESSION['login'] =  $loginIndex;
         $_SESSION['senha'] = $senhaIndex;
-        header("Location: home.php");
+        $_SESSION['tipo'] = $tipoIndex;
+        print_r($_SESSION['tipo']);
+        //header("Location: home.php");
     } else {
         unset($_SESSION['login']);
         unset($_SESSION['senha']);
-        header("Location: index.php");
+        echo "<script>alert('Usuário/Senha inválidos! Tente novamente.')</script>";
+        header("Refresh: 0");
     };
 } else {
     header('Location: index.php');
