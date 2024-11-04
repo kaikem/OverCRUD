ce68acdebc5d8c8ca6f76f2c36d69c27bfd9fc27
Kaike A. Maróstica <46039151+kaikem@users.noreply.github.com>
2024-11-04 13:21:06 -0300

    Update sessionverif.php
    Variáveis de usuário.

--- overcrud/sessionverif.php
+++ overcrud/sessionverif.php
@@ -6,8 +6,15 @@ session_start();
 if ((isset($_SESSION['login']) == true) && (isset($_SESSION['senha']) == true)) {
     $logado = $_SESSION['login'];
     $tipoUsu = $_SESSION['tipo'];
+
+    if ($tipoUsu == '0') {
+        $linksAdm = 'd-none';
+    } else {
+        $linksAdm = '';
+    };
 } else {
     unset($_SESSION['login']);
     unset($_SESSION['senha']);
+    unset($_SESSION['tipo']);
     header("Location: index.php");
 };
