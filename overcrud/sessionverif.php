450a7bf7547b92b63545b5aadf89f10df84ca4e1
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-11-04 11:24:16 -0300

    Update sessionverif.php
    Tipo de usuário para mostrar ou não opções.

--- overcrud/sessionverif.php
+++ overcrud/sessionverif.php
@@ -5,6 +5,7 @@ session_start();
 //VERIFICAÇÃO DE SESSÃO EXISTENTE
 if ((isset($_SESSION['login']) == true) && (isset($_SESSION['senha']) == true)) {
     $logado = $_SESSION['login'];
+    $tipoUsu = $_SESSION['tipo'];
 } else {
     unset($_SESSION['login']);
     unset($_SESSION['senha']);
