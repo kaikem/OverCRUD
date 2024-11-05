2fc7fa7e6c25be7e08dd1278f77f0700cf4cab6c
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-11-05 15:37:13 -0300

    Create footer.php
    Criação do FOOTER.

---
+++ overcrud/footer.php
@@ -0,0 +1,10 @@
+<?php
+echo "
+<footer class='mt-5 mb-2'>
+    <div class='row d-flex flex-column justify-content-center align-items-center text-center'>
+        <hr>
+        <img src='./img/logo_footer.png' alt='Overdrive Softwares e Consultoria' style='width: 20%'>
+        <a href='mailto:contato@overdriveconsultoria.com.br?subject=Assunto do email&body=Conteúdo do email que será preenchido automaticamente' class='mt-2'>contato@overdriveconsultoria.com.br</a>
+    </div>
+</footer>
+";
