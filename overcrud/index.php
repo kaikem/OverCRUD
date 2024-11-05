c1591ffe796df6e7b1e95d5258e33dfd971b0753
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-11-05 15:37:38 -0300

    Update index.php
    Colocando o FOOTER na página.

--- overcrud/index.php
+++ overcrud/index.php
@@ -81,6 +81,8 @@ unset($_SESSION['nome']);
 
             </div>
         </div>
+        <!-- FOOTER -->
+        <?php require_once 'footer.php' ?>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
