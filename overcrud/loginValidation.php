15aff0f6eb469a7bc01013b12a3f5318c5528099
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-11-05 13:24:46 -0300

    Update loginValidation.php
    Validando senha Hash.

--- overcrud/loginValidation.php
+++ overcrud/loginValidation.php
@@ -16,18 +16,18 @@ if (!empty($_POST['login']) && !empty($_POST['senha'])) {
         $tipoIndex = $usuarioIndex[0]['tipo'];
         $senhaHash = $usuarioIndex[0]['password'];
 
-        //if (password_verify($senhaIndex, $senhaHash)) {
-        $_SESSION['login'] =  $loginIndex;
-        $_SESSION['senha'] = $senhaIndex;
-        $_SESSION['tipo'] = $tipoIndex;
-        header("Location: home.php");
-        /*} else {
+        if (password_verify($senhaIndex, $senhaHash)) {
+            $_SESSION['login'] =  $loginIndex;
+            $_SESSION['senha'] = $senhaIndex;
+            $_SESSION['tipo'] = $tipoIndex;
+            header("Location: home.php");
+        } else {
             unset($_SESSION['login']);
             unset($_SESSION['senha']);
             unset($_SESSION['tipo']);
             echo "<script>alert('Usuário/Senha inválidos! Tente novamente.')</script>";
             header("Refresh: 0");
-        };*/
+        };
     } else {
         unset($_SESSION['login']);
         unset($_SESSION['senha']);
