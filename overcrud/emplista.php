a8cb1c98f133424ba7ab4738973f923c9995ca72
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-11-07 11:33:40 -0300

    Update emplista.php
    Modo de exibição tabela.

--- overcrud/emplista.php
+++ overcrud/emplista.php
@@ -7,7 +7,33 @@ require_once 'config.php';
 
 //TABELAS DO BD
 require_once 'sqltables.php';
-?>
+
+//MODAL EMPRESA
+foreach ($listaEmp as $empresa): ?>
+<div class='modal fade' id='empmodal<?= $empresa['idempresa'] ?>'>
+    <div class='modal-dialog'>
+        <div class='modal-content'>
+            <div class='modal-header'>
+                <h1 class='modal-title fs-5'><?= $empresa['nome'] ?></h1>
+                <button class='btn-close' data-bs-dismiss='modal'></button>
+            </div>
+            <div class='modal-body'>
+                <p class="mb-0 text-secondary display-6 fs-4"> <i class="fa-solid fa-briefcase"></i> CNPJ: </p>
+                <div class="mt-0 mb-2 display-6 fs-5"> <?= $empresa['cnpj']; ?> </div>
+                <p class="mb-0 text-secondary display-6 fs-4"> <i class="fa-solid fa-building"></i> Nome
+                    Fantasia: </p>
+                <div class="mt-0 mb-2 display-6 fs-5"> <?= $empresa['fantasia']; ?> </div>
+                <p class="mb-0 text-secondary display-6 fs-4"> <i class="fa-solid fa-phone"></i> Telefone:</p>
+                <div class="mt-0 mb-2 display-6 fs-5"> <?= $empresa['telefone']; ?> </div>
+                <p class="mb-0 text-secondary display-6 fs-4"> <i class="fa-solid fa-location-dot"></i>Endereço: </p>
+                <div class="mt-0 mb-2 display-6 fs-5"> <?= $empresa['endereco']; ?> </div>
+                <p class="mb-0 text-secondary display-6 fs-4"> <i class="fa-solid fa-user-tie"></i>Responsável: </p>
+                <div class="mt-0 mb-2 display-6 fs-5"> <?= $empresa['responsavel']; ?> </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php endforeach; ?>
 
 <!DOCTYPE html>
 <html lang="pt-br" data-bs-theme="dark">
@@ -33,40 +59,42 @@ require_once 'sqltables.php';
         <div class="row d-flex mt-5" id="appBody">
             <!-- TÍTULO DA SEÇÃO -->
             <h1 class="text-center display-6 my-5">LISTA DE EMPRESAS</h1>
-            <!-- LISTA -->
-            <?php foreach ($listaEmp as $empresa) : ?>
-            <div
-                class="col-12 col-md-10 col-lg-6 col-xl-4 justify-content-center <?php echo $empresa['idempresa'] == '0' ? 'd-none' : '' ?>">
-                <!-- CARD -->
-                <div class="card my-3 mx-1 shadow" id="empcard" style="min-height: 30rem;">
-                    <!-- HEADER DO CARD -->
-                    <div class="card-header d-flex text-center justify-content-center align-items-center"
-                        style="min-height: 8rem; max-height: 8rem;">
-                        <div class="d-flex flex-column">
-                            <h4 class="text-uppercase" id="empnome"> <?= $empresa['nome']; ?> </h4>
-                            <h6 class="text-secondary lead" id="empcnpj"> CNPJ: <?= $empresa['cnpj']; ?> </h6>
-                        </div>
-                    </div>
-                    <!-- CORPO DO CARD -->
-                    <div class="card-body">
-                        <div class="card-text">
-                            <p class="mb-0 text-secondary display-6 fs-4"> <i class="fa-solid fa-building"></i> Nome
-                                Fantasia: </p>
-                            <div class="mt-0 mb-2 display-6 fs-5"> <?= $empresa['fantasia']; ?> </div>
-                            <p class="mb-0 text-secondary display-6 fs-4"> <i class="fa-solid fa-phone"></i> Telefone:
-                            </p>
-                            <div class="mt-0 mb-2 display-6 fs-5"> <?= $empresa['telefone']; ?> </div>
-                            <p class="mb-0 text-secondary display-6 fs-4"> <i class="fa-solid fa-location-dot"></i>
-                                Endereço: </p>
-                            <div class="mt-0 mb-2 display-6 fs-5"> <?= $empresa['endereco']; ?> </div>
-                            <p class="mb-0 text-secondary display-6 fs-4"> <i class="fa-solid fa-user-tie"></i>
-                                Responsável: </p>
-                            <div class="mt-0 mb-2 display-6 fs-5"> <?= $empresa['responsavel']; ?> </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <?php endforeach; ?>
+            <!-- LISTA (TABELA) -->
+            <table id="ListaEmpresas" class="table table-striped">
+                <!-- TABELA (CABEÇALHO) -->
+                <tr class="table-secondary text-center">
+                    <th>NOME</th>
+                    <th>NOME FANTASIA</th>
+                    <th>CNPJ</th>
+                    <th> </th>
+                    <th> </th>
+                    <th> </th>
+                </tr>
+                <?php foreach ($listaEmp as $empresa): ?>
+                <!-- TABELA (DADOS DA EMPRESA) -->
+                <tr class="table <?php echo $empresa['idempresa'] == '0' ? 'd-none' : '' ?>">
+                    <td><?= $empresa['nome']; ?></td>
+                    <td><?= $empresa['fantasia']; ?></td>
+                    <td><?= $empresa['cnpj']; ?></td>
+                    <td>
+                        <a class="modalanchor" data-bs-toggle="modal"
+                            data-bs-target="#empmodal<?= $empresa['idempresa'] ?>">
+                            <i class="fa-solid fa-circle-info p-0" title="Detalhes" id="infoicon"></i>
+                        </a>
+                    </td>
+                    <td>
+                        <a href="emp_deletar.php?idempresa=<?= $empresa['idempresa']; ?>">
+                            <i class="fa-solid fa-trash p-0" title="Deletar" id="deleteicon"></i>
+                        </a>
+                    </td>
+                    <td>
+                        <a href="emp_editar.php?idempresa=<?= $empresa['idempresa']; ?>">
+                            <i class="fa-solid fa-pen-to-square p-0" title="Editar"></i>
+                        </a>
+                    </td>
+                </tr>
+                <?php endforeach; ?>
+            </table>
         </div>
         <!-- FOOTER -->
         <?php require_once 'footer.php' ?>
