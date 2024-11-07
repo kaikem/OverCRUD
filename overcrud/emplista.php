e65f75fd86e5f00bd7e54aecfa9dbe8d1663e6bb
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-11-07 11:51:40 -0300

    Update emplista.php
    Reorganização.

--- overcrud/emplista.php
+++ overcrud/emplista.php
@@ -13,20 +13,31 @@ foreach ($listaEmp as $empresa): ?>
 <div class='modal fade' id='empmodal<?= $empresa['idempresa'] ?>'>
     <div class='modal-dialog'>
         <div class='modal-content'>
+            <!-- HEADER DO MODAL -->
             <div class='modal-header'>
-                <h1 class='modal-title fs-5'><?= $empresa['nome'] ?></h1>
+                <h1 class='modal-title text-uppercase fs-5 '><?= $empresa['nome'] ?></h1>
                 <button class='btn-close' data-bs-dismiss='modal'></button>
             </div>
+            <!-- CORPO DO MODAL -->
             <div class='modal-body'>
+                <!-- CNPJ -->
                 <p class="mb-0 text-secondary display-6 fs-4"> <i class="fa-solid fa-briefcase"></i> CNPJ: </p>
                 <div class="mt-0 mb-2 display-6 fs-5"> <?= $empresa['cnpj']; ?> </div>
+
+                <!-- FANTASIA -->
                 <p class="mb-0 text-secondary display-6 fs-4"> <i class="fa-solid fa-building"></i> Nome
                     Fantasia: </p>
                 <div class="mt-0 mb-2 display-6 fs-5"> <?= $empresa['fantasia']; ?> </div>
+
+                <!-- TELEFONE -->
                 <p class="mb-0 text-secondary display-6 fs-4"> <i class="fa-solid fa-phone"></i> Telefone:</p>
                 <div class="mt-0 mb-2 display-6 fs-5"> <?= $empresa['telefone']; ?> </div>
+
+                <!-- ENDEREÇO -->
                 <p class="mb-0 text-secondary display-6 fs-4"> <i class="fa-solid fa-location-dot"></i>Endereço: </p>
                 <div class="mt-0 mb-2 display-6 fs-5"> <?= $empresa['endereco']; ?> </div>
+
+                <!-- RESPONSÁVEL -->
                 <p class="mb-0 text-secondary display-6 fs-4"> <i class="fa-solid fa-user-tie"></i>Responsável: </p>
                 <div class="mt-0 mb-2 display-6 fs-5"> <?= $empresa['responsavel']; ?> </div>
             </div>
