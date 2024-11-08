5367cc89b36f3c99f3f2120fdf0ab0918c727a07
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-11-08 10:07:33 -0300

    Update empMudarDisplay.js
    Display cards/tabela.

--- overcrud/js/empMudarDisplay.js
+++ overcrud/js/empMudarDisplay.js
@@ -1,22 +1,20 @@
-const cardsEmpresas = document.querySelector('.mostrarCardsEmpresas');
-const tabelaEmpresas = document.querySelector('.mostrarTabelaEmpresas');
-const botaoDisplayTabela = document.getElementById('displaytabela');
-const botaoDisplayCards = document.getElementById('displaycards');
+const cardsEmpresas = document.getElementById('mostrarCardsEmpresas');
+const tabelaEmpresas = document.getElementById('mostrarTabelaEmpresas');
+const botaoDisplayTabela = document.getElementById('btndisplaytabela');
+const botaoDisplayCards = document.getElementById('btndisplaycards');
 
 function mudarDisplayParaTabela(){
     cardsEmpresas.classList.add('d-none');
-    cardsEmpresas.classList.remove('d-flex');
-    tabelaEmpresas.classList.add('d-flex');
     tabelaEmpresas.classList.remove('d-none');
+    tabelaEmpresas.classList.add('d-flex');
     botaoDisplayTabela.classList.add('active');
     botaoDisplayCards.classList.remove('active');
 };
 
 function mudarDisplayParaCards(){
-    cardsEmpresas.classList.add('d-flex');
     cardsEmpresas.classList.remove('d-none');
-    tabelaEmpresas.classList.add('d-none');
     tabelaEmpresas.classList.remove('d-flex');
-    botaoDisplayCards.classList.add('active')
+    tabelaEmpresas.classList.add('d-none');
+    botaoDisplayCards.classList.add('active');
     botaoDisplayTabela.classList.remove('active');
 };
