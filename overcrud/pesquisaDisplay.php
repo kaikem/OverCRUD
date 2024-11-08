1d9ae7bf92095ac1499b36af45630d64c62a7852
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-11-08 10:18:53 -0300

    Create pesquisaDisplay.php
    Componentização da barra de pesquisa & display.

---
+++ overcrud/pesquisaDisplay.php
@@ -0,0 +1,21 @@
+<?php
+
+echo "
+            <div class='col-12 d-flex mb-3 justify-content-between'>
+                <!-- INPUT DE PESQUISA -->
+                <div class='input-group w-50' id='pesquisalista'>
+                    <i class='fa-solid fa-magnifying-glass input-group-text p-2'></i>
+                    <input type='search' class='form-control' placeholder='Pesquise por nome'>
+                </div>
+                <!-- BOTÕES DE MODO DE DISPLAY -->
+                <div class='btn-group btn-group-lg'>
+                    <button type='button'
+                        class='btn btn-outline-primary text-white btn-lg rounded-start-3 border-3 p-2 active'
+                        id='btndisplaytabela' onclick='mudarDisplayParaTabela()'><i
+                            class='fa-solid fa-list'></i></button>
+                    <button type='button' class='btn btn-outline-primary text-white btn-lg rounded-end-3 border-3 p-2'
+                        id='btndisplaycards' onclick='mudarDisplayParaCards()'><i
+                            class='fa-solid fa-table-cells'></i></button>
+                </div>
+            </div>
+            ";
