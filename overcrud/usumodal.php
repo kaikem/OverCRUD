1294b286eb55202fc5b5f9fef6e2e5300ca32b3f
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-11-11 11:19:08 -0300

    Update usumodal.php
    Inserção do campo EMPREGADO EM.

--- overcrud/usumodal.php
+++ overcrud/usumodal.php
@@ -1,18 +1,18 @@
 <?php
 foreach ($listaUsu as $usuario): ?>
-    <div class='modal fade' id='usumodal<?= $usuario['idusuario'] ?>'>
-        <div class='modal-dialog'>
-            <div class='modal-content'>
-                <!-- HEADER DO MODAL -->
-                <div class='modal-header'>
-                    <h1 class='modal-title text-uppercase fs-5'><?= $usuario['nome'] ?></h1>
-                    <button class='btn-close' data-bs-dismiss='modal'></button>
-                </div>
-                <!-- CORPO DO MODAL -->
-                <div class='modal-body'>
-                    <!-- STATUS -->
-                    <p class="mb-0 text-secondary display-6 fs-4"> <i class="fa-solid fa-chart-line"></i> Status: </p>
-                    <?php
+<div class='modal fade' id='usumodal<?= $usuario['idusuario'] ?>'>
+    <div class='modal-dialog'>
+        <div class='modal-content'>
+            <!-- HEADER DO MODAL -->
+            <div class='modal-header'>
+                <h1 class='modal-title text-uppercase fs-5'><?= $usuario['nome'] ?></h1>
+                <button class='btn-close' data-bs-dismiss='modal'></button>
+            </div>
+            <!-- CORPO DO MODAL -->
+            <div class='modal-body'>
+                <!-- STATUS -->
+                <p class="mb-0 text-secondary display-6 fs-4"> <i class="fa-solid fa-chart-line"></i> Status: </p>
+                <?php
                     if ($usuario['status'] == 1) {
                         echo "<div class='mt-0 mb-3 display-6 fs-5 text-success'> ATIVO</div>";
                     } else {
@@ -20,68 +20,82 @@ foreach ($listaUsu as $usuario): ?>
                     };
                     ?>
 
-                    <!-- CPF -->
-                    <p class="mb-0 text-secondary display-6 fs-4"> <i class="fa-solid fa-person"></i> CPF: </p>
-                    <div class="mt-0 mb-3 display-6 fs-5"> <?= $usuario['cpf']; ?> </div>
+                <!-- CPF -->
+                <p class="mb-0 text-secondary display-6 fs-4"> <i class="fa-solid fa-person"></i> CPF: </p>
+                <div class="mt-0 mb-3 display-6 fs-5"> <?= $usuario['cpf']; ?> </div>
 
-                    <!-- TELEFONE -->
-                    <p class="mb-0 text-secondary display-6 fs-4"> <i class="fa-solid fa-phone"></i> Telefone:</p>
-                    <div class="mt-0 mb-3 display-6 fs-5">
-                        <?php
+                <!-- TELEFONE -->
+                <p class="mb-0 text-secondary display-6 fs-4"> <i class="fa-solid fa-phone"></i> Telefone:</p>
+                <div class="mt-0 mb-3 display-6 fs-5">
+                    <?php
                         if ($usuario['telefone'] == "" || $usuario['telefone'] == null) {
                             echo "- NÃO POSSUI -";
                         } else {
                             echo $usuario['telefone'];
                         }
                         ?>
-                    </div>
+                </div>
 
-                    <!-- ENDEREÇO -->
-                    <p class="mb-0 text-secondary display-6 fs-4"> <i class="fa-solid fa-location-dot"></i>
-                        Endereço: </p>
-                    <div class="mt-0 mb-3 display-6 fs-5"> <?= $usuario['endereco']; ?> </div>
+                <!-- ENDEREÇO -->
+                <p class="mb-0 text-secondary display-6 fs-4"> <i class="fa-solid fa-location-dot"></i>
+                    Endereço: </p>
+                <div class="mt-0 mb-3 display-6 fs-5"> <?= $usuario['endereco']; ?> </div>
 
-                    <!-- CNH -->
-                    <p class="mb-0 text-secondary display-6 fs-4"> <i class="fa-solid fa-id-card"></i>
-                        CNH: </p>
-                    <div class="mt-0 mb-3 display-6 fs-5">
-                        <?php
+                <!-- CNH -->
+                <p class="mb-0 text-secondary display-6 fs-4"> <i class="fa-solid fa-id-card"></i>
+                    CNH: </p>
+                <div class="mt-0 mb-3 display-6 fs-5">
+                    <?php
                         if ($usuario['cnh'] == "" || $usuario['cnh'] == null) {
                             echo "- NÃO POSSUI -";
                         } else {
                             echo $usuario['cnh'];
                         }
                         ?>
-                    </div>
+                </div>
 
-                    <!-- CARRO -->
-                    <p class="mb-0 text-secondary display-6 fs-4"> <i class="fa-solid fa-car"></i>
-                        Carro: </p>
-                    <div class="mt-0 mb-3 display-6 fs-5">
-                        <?php
+                <!-- CARRO -->
+                <p class="mb-0 text-secondary display-6 fs-4"> <i class="fa-solid fa-car"></i>
+                    Carro: </p>
+                <div class="mt-0 mb-3 display-6 fs-5">
+                    <?php
                         if ($usuario['carro'] == "" || $usuario['carro'] == null) {
                             echo "- NENHUM -";
                         } else {
                             echo $usuario['carro'];
                         }
                         ?>
-                    </div>
+                </div>
 
-                    <!-- TIPO -->
-                    <p class="mb-0 text-secondary display-6 fs-4"> <i class="fa-solid fa-file-invoice"></i>
-                        Tipo de
-                        Conta: </p>
-                    <div class="mt-0 mb-3 display-6 fs-5">
-                        <?php
+                <!-- EMPREGADO EM -->
+                <p class="cardlabel mb-0 text-secondary display-6"> <i class="fa-solid fa-briefcase"></i>
+                    Empregado em: </p>
+                <div class="cardinfo mt-0 mb-2 display-6">
+                    <?php
+                        for ($i = 0; $i < count($listaEmp); $i++) {
+                            if ($usuario['idempregadoem'] == $listaEmp[$i]['idempresa']) {
+                                echo $listaEmp[$i]['nome'];
+                            };
+                        };
+                        ?>
+                </div>
+
+                <!-- TIPO -->
+                <p class="mb-0 text-secondary display-6 fs-4"> <i class="fa-solid fa-file-invoice"></i>
+                    Tipo de
+                    Conta: </p>
+                <div class="mt-0 mb-3 display-6 fs-5">
+                    <?php
                         if ($usuario['tipo'] == 0) {
                             echo "Comum";
                         } else {
                             echo "Admin";
                         };
                         ?>
-                    </div>
                 </div>
+
             </div>
         </div>
     </div>
+</div>
 <?php endforeach; ?>
