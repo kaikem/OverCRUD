1b0d90f04e4e93e9d89579ecc73a0a1311573d3a
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-11-11 10:50:48 -0300

    Create empeditar_action.php
    Tela de confirmação de edição de dados.

---
+++ overcrud/empeditar_action.php
@@ -0,0 +1,82 @@
+<?php
+//VERIFICAÇÃO DE SESSÃO
+require_once 'sessionverif.php';
+
+//CONEXÃO COM BD
+require_once 'config.php';
+
+//FUNÇÃO DE MENSAGENS
+require_once 'support.php';
+
+//TABELAS DO BD
+require_once 'sqltables.php';
+
+//MODALS
+require_once 'empmodal.php';
+require_once 'empdeletemodal.php';
+
+//RECEBIMENTO DE DADOS DO FORMULÁRIO
+$idempresa = filter_input(INPUT_POST, 'idempresa');
+$cnpj = $_POST['cnpj'];
+$nome = $_POST['nome'];
+$fantasia = $_POST['fantasia'];
+$telefone = $_POST['telefone'];
+$endereco = $_POST['endereco'];
+$responsavel = $_POST['responsavel'];
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br" data-bs-theme="dark">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css" />
+    <title>OverCRUD - Editar Empresa</title>
+</head>
+
+<body>
+    <div class="container">
+        <!-- ROW DA NAVBAR -->
+        <div class="row" id="navbarTop">
+            <?php require_once 'navbarTop.php' ?>
+        </div>
+
+        <!-- ROW DO CORPO -->
+        <div class="row d-flex justify-content-center mt-5" id="appBody">
+
+            <!-- TÍTULO DA SEÇÃO -->
+            <h1 class="text-center text-primary display-6 my-5">LISTA DE EMPRESAS</h1>
+
+            <!-- CONFIRMAÇÃO DE EDIÇÃO -->
+            <div class="col-4 col-md-6 text-center">
+                <?php
+                //ATUALIZAÇÃO DE CAMPOS NO BANCO DE DADOS
+                $sqlAtualizar = $pdo->prepare("UPDATE empresas SET nome='$nome', telefone='$telefone', endereco='$endereco', fantasia='$fantasia', cnpj='$cnpj', responsavel='$responsavel' WHERE idempresa='$idempresa'");
+                $sqlAtualizar->execute();
+
+                mensagemRetorno("Os dados de <b>$nome (CNPJ $cnpj)</b> foram atualizados com sucesso!", "success");
+                ?>
+
+                <!-- BOTÃO VOLTAR -->
+                <a href="emplista.php" class="d-block btn btn-warning">VOLTAR</a>
+            </div>
+
+            <!-- FOOTER -->
+            <?php require_once 'footer.php' ?>
+        </div>
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+        </script>
+        <script src="https://kit.fontawesome.com/9e35ffe1bb.js" crossorigin="anonymous"></script>
+        <script src="./js/darkmodetoggle.js"></script>
+        <script src="./js/modals.js"></script>
+        <script src="./js/empMudarDisplay.js"></script>
+</body>
+
+</html>
