5e991f02fbcd2a3ac699a264fb69069dfa389f5b
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-11-12 10:53:12 -0300

    Update logout.php
    Aprimoramento do logout.

--- overcrud/logout.php
+++ overcrud/logout.php
@@ -1,3 +1,8 @@
 <?php
 session_start();
+unset($_SESSION['cpf']);
+unset($_SESSION['senha']);
+unset($_SESSION['tipo']);
+unset($_SESSION['nome']);
+$_SESSION['valido'] = true;
 header('Location: index.php');
