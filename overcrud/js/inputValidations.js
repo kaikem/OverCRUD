62057a765bdc303097783c404b9a0cdd1b7d7ed1
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-11-12 10:51:43 -0300

    Update inputValidations.js

--- overcrud/js/inputValidations.js
+++ overcrud/js/inputValidations.js
@@ -3,7 +3,7 @@ function ValidaCPF(strCPF) {
     var Soma;
     var Resto;
     Soma = 0;
-  if (strCPF == "00000000000") return false;
+  if (strCPF == "000.000.000-00") return false;
 
   for (i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
   Resto = (Soma * 10) % 11;
