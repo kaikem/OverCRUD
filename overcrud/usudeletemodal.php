dd184006f92ab0fdfed5659903299fdf31c556aa
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-11-13 11:12:08 -0300

    Update usudeletemodal.php
    Reorganização do modal.

--- overcrud/usudeletemodal.php
+++ overcrud/usudeletemodal.php
@@ -1,23 +1,29 @@
 <?php
 foreach ($listaUsu as $usuario): ?>
-<div class='modal fade' id='usudeletemodal<?= $usuario['idusuario'] ?>'>
-    <div class='modal-dialog'>
-        <div class='modal-content'>
-            <!-- HEADER DO MODAL -->
-            <div class='modal-header'>
-                <h1 class='modal-title text-uppercase fs-5'>Excluir usuário(a)</h1>
-            </div>
-            <!-- CORPO DO MODAL -->
-            <div class='modal-body'>
-                Tem certeza que quer excluir o(a) usuário(a) <b><?= $usuario['nome'] ?> (CPF:
-                    <?= $usuario['cpf'] ?>)</b>?
-            </div>
-            <div class='modal-footer'>
-                <button class='btn btn-secondary' data-bs-dismiss='modal'>Voltar</button>
-                <a class='btn btn-danger'
-                    href='usudelete_action.php?idusuario=<?= $usuario['idusuario']; ?>'>Excluir</a>
+    <div class='modal fade' id='usudeletemodal<?= $usuario['idusuario'] ?>'>
+        <div class='modal-dialog'>
+            <div class='modal-content'>
+                <!-- HEADER DO MODAL -->
+                <div class='modal-header'>
+                    <h1 class='modal-title text-uppercase fs-5'>Excluir usuário(a)</h1>
+                </div>
+                <!-- CORPO DO MODAL -->
+                <div class='modal-body'>
+                    Tem certeza que deseja excluir este(a) usuario(a)? <br><b>NOME: <?= $usuario['nome'] ?> <br>CPF:
+                        <?= $usuario['cpf'] ?></b>
+                </div>
+
+                <!-- FOOTER DO MODAL -->
+                <div class='modal-footer'>
+                    <button class='btn btn-secondary' data-bs-dismiss='modal'>Voltar</button>
+                    <form action="usudelete_action.php" method="POST" style="background-color: transparent;">
+                        <input type="hidden" name="idusuario" value="<?= $usuario['idusuario'] ?>">
+                        <div class="form-group">
+                            <input type="submit" class="btn btn-danger text-center" value="EXCLUIR">
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
 <?php endforeach; ?>
