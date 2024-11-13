4f30d07498bdc9f84751d0188604c254f02afade
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-11-13 14:49:25 -0300

    Update config.php
    Mudança de senha apra BD do Notebook.

--- overcrud/config.php
+++ overcrud/config.php
@@ -3,7 +3,8 @@
 $db_name = 'phppdo';
 $db_host = 'localhost:3307';
 $db_user = 'root';
-$db_password = 'admin123';
+//$db_password = 'admin123';
+$db_password = '';
 
 $pdo = new PDO("mysql:dbname=$db_name;host=$db_host", $db_user, $db_password);
 
