6ac153b756ed183985677d89441081ba1c55d290
Kaike A. Maróstica <46039151+kaikem@users.noreply.github.com>
2024-11-18 15:29:34 -0300

    Update sqltables.php
    lista de nomes das empresas cadastradas.

--- overcrud/sqltables.php
+++ overcrud/sqltables.php
@@ -1,16 +1,35 @@
 <?php
 //USUÁRIOS
 $listaUsu = [];
+$listaUsuNomes = [];
 
 $sqlConsultaUsu = $pdo->query("SELECT * FROM usuarios");
 if ($sqlConsultaUsu->rowCount() > 0) {
     $listaUsu = $sqlConsultaUsu->fetchAll(PDO::FETCH_ASSOC);
 };
 
+$sqlConsultaUsuNomes = $pdo->query("SELECT nome FROM usuarios");
+if ($sqlConsultaUsuNomes->rowCount() > 0) {
+    $listaUsuNomes = $sqlConsultaUsuNomes->fetchAll(PDO::FETCH_ASSOC);
+};
+
+
 //EMPRESAS
 $listaEmp = [];
+$listaEmpNomes = [];
+$listaEmpNomesSomente = [];
 
 $sqlConsultaEmp = $pdo->query("SELECT * FROM empresas");
 if ($sqlConsultaEmp->rowCount() > 0) {
     $listaEmp = $sqlConsultaEmp->fetchAll(PDO::FETCH_ASSOC);
+};
+
+$sqlConsultaEmpNomes = $pdo->query("SELECT nome FROM empresas");
+if ($sqlConsultaEmpNomes->rowCount() > 0) {
+    $listaEmpNomes = $sqlConsultaEmpNomes->fetchAll(PDO::FETCH_ASSOC);
+};
+
+foreach($listaEmpNomes as $empNome){
+    array_push($listaEmpNomesSomente, $empNome['nome']);
+    sort($listaEmpNomesSomente);
 };
