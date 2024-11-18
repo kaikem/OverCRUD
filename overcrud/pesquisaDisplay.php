6c614cddc9f99b90288ef3abb810a9fd3e63a76d
Kaike A. Maróstica <46039151+kaikem@users.noreply.github.com>
2024-11-18 15:29:16 -0300

    Update pesquisaDisplay.php
    placeholder

--- overcrud/pesquisaDisplay.php
+++ overcrud/pesquisaDisplay.php
@@ -1,12 +1,24 @@
 <?php
 
 echo "
-            <div class='col-12 d-flex mb-3 justify-content-between'>
+            <div class='col-12 d-flex mb-3 justify-content-between align-items-center'>
                 <!-- INPUT DE PESQUISA -->
                 <div class='input-group w-50' id='pesquisalista'>
                     <i class='fa-solid fa-magnifying-glass input-group-text p-2'></i>
-                    <input type='search' class='form-control' placeholder='Pesquise por nome'>
+                    <input type='search' class='form-control' id='inputpesquisa' placeholder='Pesquisa por documento'>
                 </div>
+
+                <!-- BOTÕES DE ORDENAÇÃO -->
+                <!-- ORDENAÇÃO POR NOME CRESCENTE -->
+                <div>
+                <button type='button' class='btn btn-outline-secondary rounded-3 border-2 p-2'
+                        id='btnordenarpornome' onclick='ordenarPorNomeCres()' title='Ordenar nomes A-Z'><i class='fa-solid fa-arrow-down-a-z'></i></button>
+                
+                <!-- ORDENAÇÃO POR NOME DECRESCENTE -->
+                <button type='button' class='btn btn-outline-secondary rounded-3 border-2 p-2'
+                        id='btnordenarpornome' onclick='ordenarPorNomeDecres()' title='Ordenar nomes Z-A'><i class='fa-solid fa-arrow-down-z-a'></i></button>
+                </div>
+
                 <!-- BOTÕES DE MODO DE DISPLAY -->
                 <div class='btn-group btn-group-lg'>
                     <button type='button'
@@ -19,3 +31,4 @@ echo "
                 </div>
             </div>
             ";
+
