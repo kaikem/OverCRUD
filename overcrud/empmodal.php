3488278ec3505ff024be38b3dfa4c8cc55872aaf
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-11-19 16:45:28 -0300

    Update empmodal.php
    Mudanças para mostrar o endereço completo.

--- overcrud/empmodal.php
+++ overcrud/empmodal.php
@@ -26,7 +26,12 @@ foreach ($listaEmp as $empresa): ?>
 
                 <!-- ENDEREÇO -->
                 <p class="mb-0 text-secondary display-6 fs-4"> <i class="fa-solid fa-location-dot"></i> Endereço: </p>
-                <div class="mt-0 mb-3 display-6 fs-5"> <?= $empresa['endereco']; ?> </div>
+                <div class="mt-0 display-6 fs-5"> <?= $empresa['logradouro']; ?>, nº
+                    <?= $empresa['numlogradouro']; ?> (Bairro: <?= $empresa['bairro']; ?>)
+                </div>
+                <div class="mt-0 mb-3 display-6 fs-5"> <?= $empresa['cidade']; ?> -
+                    <?= $empresa['estado']; ?>
+                </div>
 
                 <!-- RESPONSÁVEL -->
                 <p class="mb-0 text-secondary display-6 fs-4"> <i class="fa-solid fa-user-tie"></i> Responsável: </p>
