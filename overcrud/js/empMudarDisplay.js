8b994b3422eea4e2b1873f20a7f960343d3bc507
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-11-19 17:31:30 -0300

    Update empMudarDisplay.js
    Reorganização e botões de ordenação.

--- overcrud/js/empMudarDisplay.js
+++ overcrud/js/empMudarDisplay.js
@@ -2,19 +2,28 @@ const cardsEmpresas = document.getElementById('mostrarcards');
 const tabelaEmpresas = document.getElementById('mostrartabela');
 const botaoDisplayTabela = document.getElementById('btndisplaytabela');
 const botaoDisplayCards = document.getElementById('btndisplaycards');
+const botoesOrdenacao = document.getElementById('btnordem');
 
 function mudarDisplayParaTabela(){
     cardsEmpresas.classList.add('d-none');
+
+    botoesOrdenacao.classList.add('d-none');
+
     tabelaEmpresas.classList.remove('d-none');
     tabelaEmpresas.classList.add('d-flex');
+
     botaoDisplayTabela.classList.add('active');
     botaoDisplayCards.classList.remove('active');
 };
 
 function mudarDisplayParaCards(){
     cardsEmpresas.classList.remove('d-none');
+
+    botoesOrdenacao.classList.remove('d-none');
+
     tabelaEmpresas.classList.remove('d-flex');
     tabelaEmpresas.classList.add('d-none');
+
     botaoDisplayCards.classList.add('active');
     botaoDisplayTabela.classList.remove('active');
 };
