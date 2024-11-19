edb258589b3a949851e548583953e14e5ab0030a
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-11-19 11:34:03 -0300

    Update emplista.php
    Nomenclatura.

--- overcrud/emplista.php
+++ overcrud/emplista.php
@@ -43,69 +43,69 @@ require_once 'empdeletemodal.php';
             <?php require_once 'pesquisaDisplay.php' ?>
 
             <!-- LISTA (CARDS) -->
-            <div class="row d-none" id="mostrarcardsempresas">
+            <div class="row d-none" id="mostrarcards">
                 <?php foreach ($listaEmp as $empresa) : ?>
-                <div class=" col-12 col-md-10 col-lg-6 col-xl-4 justify-content-center
+                    <div class=" col-12 col-md-10 col-lg-6 col-xl-4 justify-content-center
                 <?php echo $empresa['idempresa'] == '0' ? 'd-none' : '' ?>">
-                    <!-- CARD -->
-                    <div class="card mb-4 me-1" id="empcard" style="min-height: 29.6rem;">
-                        <!-- HEADER DO CARD -->
-                        <div class="card-header d-flex text-center justify-content-center align-items-center"
-                            style="min-height: 8rem; max-height: 8rem;">
-                            <div class="d-flex flex-column">
-                                <h4 class="text-uppercase" id="empnome"> <?= $empresa['nome']; ?> </h4>
-                                <h6 class="text-secondary lead" id="empcnpj"> CNPJ: <?= $empresa['cnpj']; ?> </h6>
+                        <!-- CARD -->
+                        <div class="card mb-4 me-1" id="empcard" style="min-height: 29.6rem;">
+                            <!-- HEADER DO CARD -->
+                            <div class="card-header d-flex text-center justify-content-center align-items-center"
+                                style="min-height: 8rem; max-height: 8rem;">
+                                <div class="d-flex flex-column">
+                                    <h4 class="text-uppercase" id="empnome"> <?= $empresa['nome']; ?> </h4>
+                                    <h6 class="text-secondary lead" id="empcnpj"> CNPJ: <?= $empresa['cnpj']; ?> </h6>
+                                </div>
                             </div>
-                        </div>
-                        <!-- CORPO DO CARD -->
-                        <div class="card-body">
-                            <div class="card-text" id="cardinfo">
-                                <!-- FANTASIA -->
-                                <p class="cardlabel mb-0 text-secondary display-6"> <i class="fa-solid fa-building"></i>
-                                    Nome
-                                    Fantasia: </p>
-                                <div class="cardinfo mt-0 mb-2 display-6"> <?= $empresa['fantasia']; ?> </div>
+                            <!-- CORPO DO CARD -->
+                            <div class="card-body">
+                                <div class="card-text" id="cardinfo">
+                                    <!-- FANTASIA -->
+                                    <p class="cardlabel mb-0 text-secondary display-6"> <i class="fa-solid fa-building"></i>
+                                        Nome
+                                        Fantasia: </p>
+                                    <div class="cardinfo mt-0 mb-2 display-6"> <?= $empresa['fantasia']; ?> </div>
 
-                                <!-- TELEFONE -->
-                                <p class="cardlabel mb-0 text-secondary display-6"> <i class="fa-solid fa-phone"></i>
-                                    Telefone:
-                                </p>
-                                <div class="cardinfo mt-0 mb-2 display-6"> <?= $empresa['telefone']; ?> </div>
+                                    <!-- TELEFONE -->
+                                    <p class="cardlabel mb-0 text-secondary display-6"> <i class="fa-solid fa-phone"></i>
+                                        Telefone:
+                                    </p>
+                                    <div class="cardinfo mt-0 mb-2 display-6"> <?= $empresa['telefone']; ?> </div>
 
-                                <!-- ENDEREÇO -->
-                                <p class="cardlabel mb-0 text-secondary display-6"> <i
-                                        class="fa-solid fa-location-dot"></i>
-                                    Endereço: </p>
-                                <div class="cardinfo mt-0 mb-2 display-6"> <?= $empresa['endereco']; ?> </div>
+                                    <!-- ENDEREÇO -->
+                                    <p class="cardlabel mb-0 text-secondary display-6"> <i
+                                            class="fa-solid fa-location-dot"></i>
+                                        Endereço: </p>
+                                    <div class="cardinfo mt-0 mb-2 display-6"> <?= $empresa['endereco']; ?> </div>
 
-                                <!-- RESPONSÁVEL -->
-                                <p class="cardlabel mb-0 text-secondary display-6"> <i class="fa-solid fa-user-tie"></i>
-                                    Responsável: </p>
-                                <div class="cardinfo mt-0 mb-2 display-6"> <?= $empresa['responsavel']; ?> </div>
+                                    <!-- RESPONSÁVEL -->
+                                    <p class="cardlabel mb-0 text-secondary display-6"> <i class="fa-solid fa-user-tie"></i>
+                                        Responsável: </p>
+                                    <div class="cardinfo mt-0 mb-2 display-6"> <?= $empresa['responsavel']; ?> </div>
 
+                                </div>
                             </div>
-                        </div>
-                        <!-- BOTÕES EDITAR & EXCLUIR -->
-                        <div class="d-flex align-bottom justify-content-center gap-2 my-2 <?= $linksAdm ?>">
-                            <!-- EXCLUIR -->
-                            <a data-bs-toggle='modal' data-bs-target='#empdeletemodal<?= $empresa['idempresa'] ?>'>
-                                <i class="fa-solid fa-trash btn btn-outline-danger border-3 p-2 rounded-circle"
-                                    title="Excluir"></i>
-                            </a>
+                            <!-- BOTÕES EDITAR & EXCLUIR -->
+                            <div class="d-flex align-bottom justify-content-center gap-2 my-2 <?= $linksAdm ?>">
+                                <!-- EXCLUIR -->
+                                <a data-bs-toggle='modal' data-bs-target='#empdeletemodal<?= $empresa['idempresa'] ?>'>
+                                    <i class="fa-solid fa-trash btn btn-outline-danger border-3 p-2 rounded-circle"
+                                        title="Excluir"></i>
+                                </a>
 
-                            <!-- EDITAR -->
-                            <a href="empeditar.php?idempresa=<?= $empresa['idempresa']; ?>">
-                                <i class="fa-solid fa-pen-to-square btn btn-outline-warning border-3 p-2 rounded-circle"
-                                    title="Editar"></i>
-                            </a>
+                                <!-- EDITAR -->
+                                <a href="empeditar.php?idempresa=<?= $empresa['idempresa']; ?>">
+                                    <i class="fa-solid fa-pen-to-square btn btn-outline-warning border-3 p-2 rounded-circle"
+                                        title="Editar"></i>
+                                </a>
+                            </div>
                         </div>
                     </div>
-                </div>
                 <?php endforeach; ?>
             </div>
 
             <!-- LISTA (TABELA) -->
-            <div class="" id="mostrartabelaempresas">
+            <div class="" id="mostrartabela">
                 <table id="tabelaempresas" class="table table-striped">
                     <!-- TABELA (CABEÇALHO) -->
                     <tr class="table-secondary text-center">
@@ -117,32 +117,32 @@ require_once 'empdeletemodal.php';
                         <th class="<?= $linksAdm ?>"> </th>
                     </tr>
                     <?php foreach ($listaEmp as $empresa): ?>
-                    <!-- TABELA (DADOS DA EMPRESA) -->
-                    <tr class="table <?php echo $empresa['idempresa'] == '0' ? 'd-none' : '' ?>">
-                        <td><?= $empresa['nome']; ?></td>
-                        <td><?= $empresa['fantasia']; ?></td>
-                        <td><?= $empresa['cnpj']; ?></td>
-                        <td>
-                            <a class="modalanchor" data-bs-toggle="modal"
-                                data-bs-target="#empmodal<?= $empresa['idempresa'] ?>">
-                                <i class="fa-solid fa-circle-info text-primary px-0 py-2" title="Detalhes"></i>
-                            </a>
-                        </td>
-                        <!-- BOTÕES EDITAR & EXCLUIR -->
-                        <!-- EXCLUIR -->
-                        <td class="<?= $linksAdm ?>">
-                            <a data-bs-toggle='modal' data-bs-target='#empdeletemodal<?= $empresa['idempresa'] ?>'>
-                                <i class="fa-solid fa-trash px-0 py-2" title="Excluir" id="deleteicon"></i>
-                            </a>
-                        </td>
+                        <!-- TABELA (DADOS DA EMPRESA) -->
+                        <tr class="table <?php echo $empresa['idempresa'] == '0' ? 'd-none' : '' ?>">
+                            <td><?= $empresa['nome']; ?></td>
+                            <td><?= $empresa['fantasia']; ?></td>
+                            <td><?= $empresa['cnpj']; ?></td>
+                            <td>
+                                <a class="modalanchor" data-bs-toggle="modal"
+                                    data-bs-target="#empmodal<?= $empresa['idempresa'] ?>">
+                                    <i class="fa-solid fa-circle-info text-primary px-0 py-2" title="Detalhes"></i>
+                                </a>
+                            </td>
+                            <!-- BOTÕES EDITAR & EXCLUIR -->
+                            <!-- EXCLUIR -->
+                            <td class="<?= $linksAdm ?>">
+                                <a data-bs-toggle='modal' data-bs-target='#empdeletemodal<?= $empresa['idempresa'] ?>'>
+                                    <i class="fa-solid fa-trash px-0 py-2" title="Excluir" id="deleteicon"></i>
+                                </a>
+                            </td>
 
-                        <!-- EDITAR -->
-                        <td class="<?= $linksAdm ?>">
-                            <a href="empeditar.php?idempresa=<?= $empresa['idempresa']; ?>">
-                                <i class="fa-solid fa-pen-to-square px-0 py-2" title="Editar"></i>
-                            </a>
-                        </td>
-                    </tr>
+                            <!-- EDITAR -->
+                            <td class="<?= $linksAdm ?>">
+                                <a href="empeditar.php?idempresa=<?= $empresa['idempresa']; ?>">
+                                    <i class="fa-solid fa-pen-to-square px-0 py-2" title="Editar"></i>
+                                </a>
+                            </td>
+                        </tr>
                     <?php endforeach; ?>
                 </table>
             </div>
