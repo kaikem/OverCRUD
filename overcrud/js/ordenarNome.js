4a0b0ab3cf53f7b192dfa68fe051c9ea019964cf
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-11-19 12:26:35 -0300

    Update ordenarNome.js
    Função JS para ordenação pelos THs.

--- overcrud/js/ordenarNome.js
+++ overcrud/js/ordenarNome.js
@@ -1,5 +1,4 @@
-var tabelaSelecionada;
-
+//DOM - TABELAS USU E EMP
 const tabelaDeEmpresas = document.getElementById('tabelaempresas');
 if(tabelaDeEmpresas !== null){
     var linhasTabelaEmpresas = tabelaDeEmpresas.rows;
@@ -10,6 +9,86 @@ if(tabelaDeUsuarios !== null){
     var linhasTabelaUsuarios = tabelaDeUsuarios.rows;
 };
 
+
+//FUNÇÕES
+function ordenar(n){
+    var switching, i, elemento1, elemento2, shouldSwitch, direcao, switchCount = 0;
+    switching = true
+    direcao = "asc";
+
+    if(tabelaDeEmpresas !== null){
+        while (switching){
+            switching = false;
+    
+            for (i = 1; i < (linhasTabelaEmpresas.length - 1); i++) {
+                shouldSwitch = false;
+    
+                elemento1 = linhasTabelaEmpresas[i].getElementsByTagName("TD")[n];
+                elemento2 = linhasTabelaEmpresas[i + 1].getElementsByTagName("TD")[n];
+    
+                if (direcao == "asc") {
+                    if (elemento1.innerHTML.toLowerCase() > elemento2.innerHTML.toLowerCase()) {
+                        shouldSwitch = true;
+                        break;
+                    };
+                } else if (direcao == "desc") {
+                    if (elemento1.innerHTML.toLowerCase() < elemento2.innerHTML.toLowerCase()) {
+                        shouldSwitch = true;
+                        break;
+                    };
+                };
+            };
+    
+            if (shouldSwitch) {
+                linhasTabelaEmpresas[i].parentNode.insertBefore(linhasTabelaEmpresas[i + 1], linhasTabelaEmpresas[i]);
+                switching = true;
+                switchCount ++;
+            } else {
+                if (switchCount == 0 && direcao == "asc") {
+                    direcao = "desc";
+                  switching = true;
+                };
+            };
+        };
+    } else {
+        while (switching){
+            switching = false;
+    
+            for (i = 1; i < (linhasTabelaUsuarios.length - 1); i++) {
+                shouldSwitch = false;
+    
+                elemento1 = linhasTabelaUsuarios[i].getElementsByTagName("TD")[n];
+                elemento2 = linhasTabelaUsuarios[i + 1].getElementsByTagName("TD")[n];
+    
+                if (direcao == "asc") {
+                    if (elemento1.innerHTML.toLowerCase() > elemento2.innerHTML.toLowerCase()) {
+                        shouldSwitch = true;
+                        break;
+                    };
+                } else if (direcao == "desc") {
+                    if (elemento1.innerHTML.toLowerCase() < elemento2.innerHTML.toLowerCase()) {
+                        shouldSwitch = true;
+                        break;
+                    };
+                };
+            };
+    
+            if (shouldSwitch) {
+                linhasTabelaUsuarios[i].parentNode.insertBefore(linhasTabelaUsuarios[i + 1], linhasTabelaUsuarios[i]);
+                switching = true;
+                switchCount ++;
+            } else {
+                if (switchCount == 0 && direcao == "asc") {
+                    direcao = "desc";
+                  switching = true;
+                };
+            };
+        };
+    }
+};
+
+
+/*
 function ordenarPorNomeCres(){
     var shouldSwitch, elemento1, elemento2;
     var switching = true;
@@ -99,3 +178,4 @@ function ordenarPorNomeDecres(){
         };
     }
 };
+*/
