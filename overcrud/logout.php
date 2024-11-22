427ba99cd11a0179fcf593929acdc9ba517a19c5
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-11-22 16:01:04 -0300

    Update logout.php
    Mudanças para exibição correta da mensagem de erro de login.

--- overcrud/logout.php
+++ overcrud/logout.php
@@ -4,7 +4,7 @@ unset($_SESSION['cpf']);
 unset($_SESSION['senha']);
 unset($_SESSION['tipo']);
 unset($_SESSION['nome']);
-$_SESSION['valido'] = true;
+$_SESSION['valido'] = "validado";
 
 if (basename($_SERVER['SCRIPT_NAME']) !== 'index.php') {
     header('Location: index.php');
