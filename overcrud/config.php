733982a1cccff59a435fa658ec0bc2451584e46d
Kaike A. Maróstica <46039151+kaikem@users.noreply.github.com>
2024-11-25 13:49:23 -0300

    Update config.php
    Mudança de servidor.

--- overcrud/config.php
+++ overcrud/config.php
@@ -1,5 +1,5 @@
 <?php
-//BD SHANGAI ESCRITÓRIO
+/*BD SHANGAI ESCRITÓRIO
 $db_name = 'phppdo';
 $db_host = 'localhost:3307';
 $db_user = 'root';
@@ -7,9 +7,10 @@ $db_user = 'root';
 $db_password = '';
 
 $pdo = new PDO("mysql:dbname=$db_name;host=$db_host", $db_user, $db_password);
+*/
 
 
-/*BD CASA
+//BD CASA
 $db_name = 'phppdo';
 $db_host = 'localhost:3306';
 $db_user = 'root';
@@ -17,4 +18,3 @@ $db_password = '';
 
 $pdo = new PDO("mysql:dbname=$db_name;host=$db_host", $db_user, $db_password);
 ?>
-*/
