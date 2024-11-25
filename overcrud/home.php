022b4afae5c8448a4202601e45746328f103fdb5
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-11-25 11:20:55 -0300

    Update home.php
    jQuery

--- overcrud/home.php
+++ overcrud/home.php
@@ -86,6 +86,7 @@ require_once 'sqltables.php';
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
         </script>
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
         <script src="https://kit.fontawesome.com/9e35ffe1bb.js" crossorigin="anonymous"></script>
         <script src="./js/darkmodetoggle.js"></script>
 </body>
