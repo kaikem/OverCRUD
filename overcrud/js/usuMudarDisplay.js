e7f1a05f6fdc2b3dacee1601a24bd1a4038b9862
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-11-25 11:21:55 -0300

    Update usuMudarDisplay.js
    Mudança de nomes de variáveis para pesquisa.

--- overcrud/js/usuMudarDisplay.js
+++ overcrud/js/usuMudarDisplay.js
@@ -1,28 +1,28 @@
-const cardsUsuarios = document.getElementById('mostrarcards');
-const tabelaUsuarios = document.getElementById('mostrartabela');
+const divCards = document.getElementById('mostrarcards');
+const divTabela = document.getElementById('mostrartabela');
 const botaoDisplayTabela = document.getElementById('btndisplaytabela');
 const botaoDisplayCards = document.getElementById('btndisplaycards');
 const botoesOrdenacao = document.getElementById('btnordem');
 
 function mudarDisplayParaTabela(){
-    cardsUsuarios.classList.add('d-none');
+    divCards.classList.add('d-none');
 
     botoesOrdenacao.classList.add('d-none');
 
-    tabelaUsuarios.classList.remove('d-none');
-    tabelaUsuarios.classList.add('d-flex');
+    divTabela.classList.remove('d-none');
+    divTabela.classList.add('d-flex');
 
     botaoDisplayTabela.classList.add('active');
     botaoDisplayCards.classList.remove('active');
 };
 
 function mudarDisplayParaCards(){
-    cardsUsuarios.classList.remove('d-none');
+    divCards.classList.remove('d-none');
 
     botoesOrdenacao.classList.remove('d-none');
 
-    tabelaUsuarios.classList.remove('d-flex');
-    tabelaUsuarios.classList.add('d-none');
+    divTabela.classList.remove('d-flex');
+    divTabela.classList.add('d-none');
 
     botaoDisplayCards.classList.add('active');
     botaoDisplayTabela.classList.remove('active');
