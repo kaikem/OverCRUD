c791d62b0c2c6509163f0b5d23c120122c34f012
Kaike A. Maróstica <46039151+kaikem@users.noreply.github.com>
2024-11-25 14:15:54 -0300

    Update footer.php
    Tamanho do logo Overdrive.

--- overcrud/footer.php
+++ overcrud/footer.php
@@ -3,7 +3,7 @@ echo "
 <footer class='mt-5 mb-2'>
     <div class='row d-flex flex-column justify-content-center align-items-center text-center'>
         <hr>
-        <img src='./img/logo_footer.png' alt='Overdrive Softwares e Consultoria' style='width: 20%'>
+        <img src='./img/logo_footer.png' alt='Overdrive Softwares e Consultoria' style='width: 18rem'>
         <a href='mailto:contato@overdriveconsultoria.com.br?subject=Assunto do email&body=Conteúdo do email que será preenchido automaticamente' class='mt-2' id='emailfooter'>contato@overdriveconsultoria.com.br</a>
     </div>
 </footer>
