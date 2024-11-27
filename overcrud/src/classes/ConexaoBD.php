bf2f7ad8194d463d85496c10feeb00387ef81d59
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-11-27 08:23:48 -0300

    Create ConexaoBD.php
    Classe de conexão com o BD.

---
+++ overcrud/src/classes/ConexaoBD.php
@@ -0,0 +1,34 @@
+<?php
+class ConexaoBD
+{
+    //-----------------------------------------------------------------------------
+    //CONSTRUTOR
+    public function __construct() {}
+
+
+    //-----------------------------------------------------------------------------
+    //FUNÇÕES
+    //conexão com bd
+    public static function conectarBD()
+    {
+        $db_name = 'phppdo';
+        $db_host = 'localhost:3307';
+        $db_user = 'root';
+        $db_port = '3307';
+        $db_password = 'admin123';
+        //$db_password = '';
+        $pdo = new PDO("mysql:dbname=$db_name;host=$db_host", $db_user, $db_password);
+        return $pdo;
+
+        /*BD CASA
+        $db_name = 'phppdo';
+        $db_host = 'localhost';
+        $db_user = 'root';
+        $db_port = '3306';
+        $db_password = '';        
+        $pdo = new PDO("mysql:dbname=$db_name;host=$db_host", $db_user, $db_password);
+        return $pdo;
+        ?>
+*/
+}
+};
