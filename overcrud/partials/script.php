930566a1f48800ac7b294066fb609426d6b7a466
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-11-27 18:38:44 -0300

    Create script.php
    Início de componente de scripts.

---
+++ overcrud/partials/script.php
@@ -0,0 +1,38 @@
+<!-- BOOTSTRAP JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+</script>
+
+<!-- FONT AWESOME -->
+<script src="https://kit.fontawesome.com/9e35ffe1bb.js" crossorigin="anonymous"></script>
+
+<!-- JQUERY -->
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+
+<!-- JQUERY MASKS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+
+<!-- DARKMODE -->
+<script src="./js/darkmodetoggle.js"></script>
+
+<!-- VALIDAÇÃO FORM EMP -->
+<script src="./js/empFormValidations.js"></script>
+
+<!-- VALIDAÇÃO FORM USU -->
+<script src="./js/usuFormValidations.js"></script>
+
+<!-- BUSCA CEP -->
+<script src="./js/buscaCep.js"></script>
+
+<!-- MUDAR DISPLAY -->
+<script src="./js/mudarDisplay.js"></script>
+
+<!-- BOOTSTRAP MODALS -->
+<script src="./js/modals.js"></script>
+
+<!-- ORDENAR POR NOME -->
+<script src="./js/ordenarNome.js"></script>
+
+<!-- PESQUISA -->
+<script src="./js/search.js"></script>
+
