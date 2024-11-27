e961798b2c5dc05cd29954a1770eb8fc95f9b095
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-11-27 08:23:37 -0300

    Update sqltables.php
    Transferência das informações para os arquivos de classes.

--- overcrud/sqltables.php
+++ overcrud/sqltables.php
@@ -1,21 +1,8 @@
 <?php
-//USUÁRIOS
-$listaUsu = [];
+/*USUÁRIOS
 $listaUsuDesc = [];
-$listaUsuNomes = [];
-$listaUsuNomesSomente = [];
 $listaUsuAdmins = [];
 $listaUsuAtivos = [];
-
-$sqlConsultaUsu = $pdo->query("SELECT * FROM usuarios");
-if ($sqlConsultaUsu->rowCount() > 0) {
-    $listaUsu = $sqlConsultaUsu->fetchAll(PDO::FETCH_ASSOC);
-};
-
-$sqlConsultaUsuNomes = $pdo->query("SELECT nome FROM usuarios");
-if ($sqlConsultaUsuNomes->rowCount() > 0) {
-    $listaUsuNomes = $sqlConsultaUsuNomes->fetchAll(PDO::FETCH_ASSOC);
-};
 
 $sqlConsultaUsuAdmin = $pdo->query("SELECT * FROM usuarios WHERE tipo='1'");
 if ($sqlConsultaUsuAdmin->rowCount() > 0) {
@@ -27,24 +14,14 @@ if ($sqlConsultaUsuStatus->rowCount() > 0) {
     $listaUsuAtivos = $sqlConsultaUsuStatus->fetchAll(PDO::FETCH_ASSOC);
 };
 
-foreach ($listaUsuNomes as $usuNome) {
-    array_push($listaUsuNomesSomente, $usuNome['nome']);
-};
-
 $listaUsuOrdenada = array_column($listaUsu, 'nome');
 //array_multisort($listaUsuOrdenada, SORT_ASC, $listaUsu);
 
 
 //EMPRESAS
-$listaEmp = [];
 $listaEmpDesc = [];
 $listaEmpNomes = [];
 $listaEmpNomesSomente = [];
-
-$sqlConsultaEmp = $pdo->query("SELECT * FROM empresas");
-if ($sqlConsultaEmp->rowCount() > 0) {
-    $listaEmp = $sqlConsultaEmp->fetchAll(PDO::FETCH_ASSOC);
-};
 
 $sqlConsultaEmpNomes = $pdo->query("SELECT nome FROM empresas");
 if ($sqlConsultaEmpNomes->rowCount() > 0) {
@@ -69,3 +46,4 @@ function OrdernarDecr($listaOrdenada, $listaFinal)
 {
     array_multisort($listaOrdenada, SORT_DESC, $listaFinal);
 };
+*/
