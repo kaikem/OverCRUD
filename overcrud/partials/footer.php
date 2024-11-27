1e99920c5e3e0833b0d3f0ad30a233f348380940
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-11-27 13:54:19 -0300

    Update footer.php
    Remoção de demais dados do email.

--- overcrud/partials/footer.php
+++ overcrud/partials/footer.php
@@ -4,7 +4,7 @@ echo "
     <div class='row d-flex flex-column justify-content-center align-items-center text-center'>
         <hr>
         <img src='./img/logo_footer.png' alt='Overdrive Softwares e Consultoria' style='width: 18rem'>
-        <a href='mailto:contato@overdriveconsultoria.com.br?subject=Assunto do email&body=Conteúdo do email que será preenchido automaticamente' class='mt-2' id='emailfooter'>contato@overdriveconsultoria.com.br</a>
+        <a href='mailto:contato@overdriveconsultoria.com.br' class='mt-2' id='emailfooter'>contato@overdriveconsultoria.com.br</a>
     </div>
 </footer>
 ";
