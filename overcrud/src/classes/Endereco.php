2eec56efd4aebf9f4c0a97b1e2e0886ee99ac793
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-11-27 08:24:07 -0300

    Create Endereco.php
    Classe Endereço.

---
+++ overcrud/src/classes/Endereco.php
@@ -0,0 +1,91 @@
+<?php
+require_once 'ConexaoBD.php';
+
+class Endereco
+{
+    private string $cep;
+    private string $cidade;
+    private string $estado;
+    private string $logradouro;
+    private int $numlogradouro;
+    private string $bairro;
+
+    //-----------------------------------------------------------------------------
+    //CONSTRUTOR
+    public function __construct() {}
+
+
+    //-----------------------------------------------------------------------------
+    //GETTERS E SETTERS
+    //cep
+    public function getCep()
+    {
+        return $this->cep;
+    }
+    public function setCep($cep)
+    {
+        $this->cep = $cep;
+
+        return $this;
+    }
+
+    //cidade
+    public function getCidade()
+    {
+        return $this->cidade;
+    }
+    public function setCidade($cidade)
+    {
+        $this->cidade = $cidade;
+
+        return $this;
+    }
+
+    //estado
+    public function getEstado()
+    {
+        return $this->estado;
+    }
+    public function setEstado($estado)
+    {
+        $this->estado = $estado;
+
+        return $this;
+    }
+
+    //logradouro
+    public function getLogradouro()
+    {
+        return $this->logradouro;
+    }
+    public function setLogradouro($logradouro)
+    {
+        $this->logradouro = $logradouro;
+
+        return $this;
+    }
+
+    //numlogradouro
+    public function getNumlogradouro()
+    {
+        return $this->numlogradouro;
+    }
+    public function setNumlogradouro($numlogradouro)
+    {
+        $this->numlogradouro = $numlogradouro;
+
+        return $this;
+    }
+
+    //bairro
+    public function getBairro()
+    {
+        return $this->bairro;
+    }
+    public function setBairro($bairro)
+    {
+        $this->bairro = $bairro;
+
+        return $this;
+    }
+}
