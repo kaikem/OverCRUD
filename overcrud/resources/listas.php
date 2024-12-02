915c3f39a5d4e8a23c39aa6ba12725dd1a5be88a
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-12-02 13:07:17 -0300

    Métodos das classes DAO
    Mudança de métodos próprios para DAO.

--- overcrud/resources/listas.php
+++ overcrud/resources/listas.php
@@ -3,39 +3,51 @@
 $root = $_SERVER["DOCUMENT_ROOT"];
 require_once "$root/overcrud/pathing.php";
 
-require_once "$rootOvercrud/components/Empresa.php";
-require_once "$rootOvercrud/components/Usuario.php";
-require_once "$rootOvercrud/components/Endereco.php";
 
 //-----------------------------------------------------------------------------
-//INSTÂNCIAS
-$instanciaEmp = new Empresa();
-$instanciaUsu = new Usuario();
-$instanciaEnd = new Endereco();
+//REQUISIÇÕES DE CLASSES
+require_once "$rootOvercrud/components/Empresa.php";
+require_once "$rootOvercrud/components/EmpresaDAO.php";
+require_once "$rootOvercrud/components/Usuario.php";
+require_once "$rootOvercrud/components/UsuarioDAO.php";
+require_once "$rootOvercrud/components/Endereco.php";
+require_once "$rootOvercrud/components/EnderecoDAO.php";
 
 
 //-----------------------------------------------------------------------------
 //EMPRESAS
+//Instâncias
+$instanciaEmp = new Empresa();
+$instanciaEmpDAO = new EmpresaDAO($instanciaEmp);
+
 //Lista Completa de Empresas
-$listaEmp = $instanciaEmp->listarEmpresas();
+$listaEmp = $instanciaEmpDAO->listarEmpresas();
 
 //Lista de Empresas Vinculadas
-$listaEmpVinculadas = $instanciaEmp->listarEmpresasVinculadas();
+$listaEmpVinculadas = $instanciaEmpDAO->listarEmpresasVinculadas();
 
 
 //-----------------------------------------------------------------------------
 //USUÁRIOS
+//Instâncias
+$instanciaUsu = new Usuario();
+$instanciaUsuDAO = new UsuarioDAO($instanciaUsu);
+
 //Lista Completa de Usuários
-$listaUsu = $instanciaUsu->listarUsuarios();
+$listaUsu = $instanciaUsuDAO->listarUsuarios();
 
 //Lista de Usuários Admins
-$listaUsuAdmins = $instanciaUsu->listarUsuariosAdm();
+$listaUsuAdmins = $instanciaUsuDAO->listarUsuariosAdm();
 
 //Lista de Usuários Ativos
-$listaUsuAtivos = $instanciaUsu->listarUsuariosAtivos();
+$listaUsuAtivos = $instanciaUsuDAO->listarUsuariosAtivos();
 
 
 //-----------------------------------------------------------------------------
 //ENDEREÇOS
+//Instâncias
+$instanciaEnd = new Endereco();
+$instanciaEndDAO = new EnderecoDAO($instanciaEnd);
+
 //Lista Completa de Endereços
-$listaEnd = $instanciaEnd->listarEnderecos();
+$listaEnd = $instanciaEndDAO->listarEnderecos();
