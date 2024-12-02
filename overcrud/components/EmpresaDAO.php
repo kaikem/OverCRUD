e7ea6bcbdae0db264dce1378dfc537a7c14f5113
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-12-02 11:38:11 -0300

    Update EmpresaDAO.php
    Estrutura básica da classe EmpresaDAO.

--- overcrud/components/EmpresaDAO.php
+++ overcrud/components/EmpresaDAO.php
@@ -1,5 +1,14 @@
-<?php 
-class EmpresaDAO{
-    
+<?php
+require_once 'Empresa.php';
+
+class EmpresaDAO
+{
+    private Empresa $empresa;
+
+    //-----------------------------------------------------------------------------
+    //CONSTRUTOR
+    public function __construct($empresaCon)
+    {
+        $this->empresa->$empresaCon;
+    }
 }
-?>
