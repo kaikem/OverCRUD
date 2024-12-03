8dea50c8838121e9370a4bdce0344e735ba83c21
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-12-03 14:40:19 -0300

    Update UsuarioDAO.php
    Funções para exclusão de usuários.

--- overcrud/components/UsuarioDAO.php
+++ overcrud/components/UsuarioDAO.php
@@ -1,11 +1,12 @@
-<?php 
+<?php
 require_once 'Usuario.php';
 
-class UsuarioDAO{
+class UsuarioDAO
+{
     //ATRIBUTOS
     private Usuario $usuario;
 
-    
+
     //-----------------------------------------------------------------------------
     //CONSTRUTOR
     public function __construct($usuarioCon)
@@ -17,10 +18,12 @@ class UsuarioDAO{
     //-----------------------------------------------------------------------------
     //GETTERS E SETTERS
     //usuario
-    public function getUsuario(){
+    public function getUsuario()
+    {
         return $this->usuario;
     }
-    public function setUsuario($usuarioSet){
+    public function setUsuario($usuarioSet)
+    {
         $this->usuario = $usuarioSet;
     }
 
@@ -32,31 +35,31 @@ class UsuarioDAO{
     {
         $listaUsu = [];
         $sqlConsultaUsu = ConexaoBD::conectarBD()->query("SELECT * FROM usuarios");
-    
+
         if ($sqlConsultaUsu->rowCount() > 0) {
             $listaUsu = $sqlConsultaUsu->fetchAll(PDO::FETCH_ASSOC);
             return $listaUsu;
         };
     }
-    
+
     //lista de usuários admins
     public function listarUsuariosAdm()
     {
         $listaUsuAdmins = [];
         $sqlConsultaUsuAdmin = ConexaoBD::conectarBD()->query("SELECT * FROM usuarios WHERE tipo='1'");
-    
+
         if ($sqlConsultaUsuAdmin->rowCount() > 0) {
             $listaUsuAdmins = $sqlConsultaUsuAdmin->fetchAll(PDO::FETCH_ASSOC);
             return $listaUsuAdmins;
         };
     }
-    
+
     //lista de usuários ativos
     public function listarUsuariosAtivos()
     {
         $listaUsuAtivos = [];
         $sqlConsultaUsuativos = ConexaoBD::conectarBD()->query("SELECT * FROM usuarios WHERE status='1'");
-    
+
         if ($sqlConsultaUsuativos->rowCount() > 0) {
             $listaUsuAtivos = $sqlConsultaUsuativos->fetchAll(PDO::FETCH_ASSOC);
             return $listaUsuAtivos;
@@ -66,7 +69,7 @@ class UsuarioDAO{
     //buscar empresa por cpf
     public function buscarPorCpf($cpf)
     {
-        $sqlVerifCpf = ConexaoBD::conectarBD()->query("SELECT * FROM empresas WHERE `cpf`='$cpf'");
+        $sqlVerifCpf = ConexaoBD::conectarBD()->query("SELECT * FROM usuarios WHERE `cpf`='$cpf'");
         return $sqlVerifCpf;
     }
 
@@ -76,5 +79,35 @@ class UsuarioDAO{
         $sqlVerifCnh = ConexaoBD::conectarBD()->query("SELECT * FROM usuarios WHERE `cnh`='$cnh' AND `cnh`!=''");
         return $sqlVerifCnh;
     }
+
+
+    //preparar para inserção de usuario no banco de dados
+    public function prepInserirUsu()
+    {
+        $sqlInserirUsu = ConexaoBD::conectarBD()->prepare("INSERT INTO usuarios (nome, telefone, cpf, password, cnh, carro, tipo, status, idempregadoem, idenderecousu) VALUES ('{$this->getUsuario()->getNome()}', '{$this->getUsuario()->getTelefone()}', '{$this->getUsuario()->getCpf()}', '{$this->getUsuario()->getPassword()}', '{$this->getUsuario()->getCnh()}', '{$this->getUsuario()->getCarro()}', '{$this->getUsuario()->getTipo()}', '{$this->getUsuario()->getStatus()}','{$this->getUsuario()->getIdempregadoem()}', '{$this->getUsuario()->getIdenderecousu()}')");
+        return $sqlInserirUsu;
+    }
+
+    //consultar usuario por id
+    public function consultaDeIdUsu()
+    {
+        $sqlConsultaIdUsu = ConexaoBD::conectarBD()->query("SELECT * FROM usuarios WHERE idusuario='{$this->getUsuario()->getIdusuario()}'");
+        return $sqlConsultaIdUsu;
+    }
+
+    //consultar usuario por id
+    public function UsuPorId()
+    {
+        $sqlConsultaIdEmp = ConexaoBD::conectarBD()->query("SELECT * FROM usuarios WHERE idusuario='{$this->getUsuario()->getIdusuario()}'");
+
+        $usuario = $sqlConsultaIdEmp->fetch(PDO::FETCH_ASSOC);
+        return $usuario;
+    }
+
+    //consultar empresa por id
+    public function excluirUsu()
+    {
+        $sqlExcluirUsu = ConexaoBD::conectarBD()->prepare("DELETE FROM usuarios WHERE idusuario='{$this->getUsuario()->getIdusuario()}'");
+        $sqlExcluirUsu->execute();
+    }
 };
-?>
