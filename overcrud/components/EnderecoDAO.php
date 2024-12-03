eb8d51e83d13cbedb0b835c1143c8bd4302d7957
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-12-03 11:33:07 -0300

    Update EnderecoDAO.php
    Funções de interação com banco de dados de endereços.

--- overcrud/components/EnderecoDAO.php
+++ overcrud/components/EnderecoDAO.php
@@ -1,7 +1,8 @@
-<?php 
+<?php
 require_once 'Endereco.php';
 
-class EnderecoDAO{
+class EnderecoDAO
+{
     //ATRIBUTOS
     private Endereco $endereco;
 
@@ -15,17 +16,58 @@ class EnderecoDAO{
 
 
     //-----------------------------------------------------------------------------
+    //GETTERS E SETTERS
+    //endereço
+    public function getEndereco()
+    {
+        return $this->endereco;
+    }
+    public function setEndereco($enderecoSet)
+    {
+        $this->endereco = $enderecoSet;
+
+        return $this;
+    }
+
+
+    //-----------------------------------------------------------------------------
     //FUNÇÕES PRÓPIRAS
     //listar endereços
     public function listarEnderecos()
     {
         $listaEnd = [];
         $sqlConsultaEnd = ConexaoBD::conectarBD()->query("SELECT * FROM enderecos");
-    
+
         if ($sqlConsultaEnd->rowCount() > 0) {
             $listaEnd = $sqlConsultaEnd->fetchAll(PDO::FETCH_ASSOC);
             return $listaEnd;
         };
     }
-}
-?>
+
+    //busca endereço específico
+    public function buscarEnd()
+    {
+        $sqlVerifEnd = ConexaoBD::conectarBD()->query("SELECT * FROM enderecos WHERE `cep`='{$this->getEndereco()->getCep()}' AND `cidade`='{$this->getEndereco()->getCidade()}' AND `estado`='{$this->getEndereco()->getEstado()}' AND `logradouro`='{$this->getEndereco()->getLogradouro()}' AND `numlogradouro`='{$this->getEndereco()->getNumlogradouro()}' AND `bairro`='{$this->getEndereco()->getBairro()}'");
+
+        return $sqlVerifEnd;
+    }
+
+    //retorno de idendereco
+    public function buscarIdEnd()
+    {
+        $sqlGetIdEnd = ConexaoBD::conectarBD()->query("SELECT MIN(idendereco) FROM enderecos WHERE `cep`='{$this->getEndereco()->getCep()}' AND `cidade`='{$this->getEndereco()->getCidade()}' AND `estado`='{$this->getEndereco()->getEstado()}' AND `logradouro`='{$this->getEndereco()->getLogradouro()}' AND `numlogradouro`='{$this->getEndereco()->getNumlogradouro()}' AND `bairro`='{$this->getEndereco()->getBairro()}'");
+
+        if ($sqlGetIdEnd->rowCount() > 0) {
+            $idendereco = $sqlGetIdEnd->fetchAll(PDO::FETCH_ASSOC);
+            $idenderecoempDAO = $idendereco[0]["MIN(idendereco)"];
+            return $idenderecoempDAO;
+        };
+    }
+
+    //inserção no banco de dados
+    public function inserirNovoEnd()
+    {
+        $sqlInserirEnd = ConexaoBD::conectarBD()->prepare("INSERT INTO enderecos (cep, cidade, estado, logradouro, numlogradouro, bairro) VALUES ('{$this->getEndereco()->getCep()}', '{$this->getEndereco()->getCidade()}', '{$this->getEndereco()->getEstado()}', '{$this->getEndereco()->getLogradouro()}', '{$this->getEndereco()->getNumlogradouro()}', '{$this->getEndereco()->getBairro()}')");
+        $sqlInserirEnd->execute();
+    }
+};
