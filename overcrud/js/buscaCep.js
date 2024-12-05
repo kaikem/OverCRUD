57d262634510e7ecf287f60f4921511ebcf42a27
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-12-05 17:20:20 -0300

    Update buscaCep.js
    Exclusão de variáveis que não estavam sendo usadas.

--- overcrud/js/buscaCep.js
+++ overcrud/js/buscaCep.js
@@ -3,7 +3,6 @@ const form = document.querySelector('.needs-validation');
 function buscaCep() {
     let inputCep = document.querySelector('input[name=cep]');
     let cep = inputCep.value.replace(/[^0-9]/g, '');
-    const cep_teste = document.getElementById('cepTesteUser');
 
     if(cep.length == 8) {
         let url = 'http://viacep.com.br/ws/' + cep + '/json';
@@ -17,28 +16,20 @@ function buscaCep() {
         };
         xhr.send();
     } else {
-        //cep_teste.innerText = 'CEP inválido'
-        //cep_teste.style.color = 'var(--red-dark)'
-
-        document.querySelector('input[name=logradouro]').value = ''
-        document.querySelector('input[name=bairro]').value = ''
-        document.querySelector('input[name=cidadeestado]').value = ''
-        document.querySelector('select[name=estadocidade]').value = ''
+        document.querySelector('input[name=logradouro]').value = '';
+        document.querySelector('input[name=bairro]').value = '';
+        document.querySelector('input[name=cidadeestado]').value = '';
+        document.querySelector('select[name=estadocidade]').value = '';
     }
 }
 
 function preencheCampos(json) {
-    //onst cep_teste = document.getElementById('cepTesteUser')
-
     if(json.localidade !== undefined){
-        //cep_teste.innerText = 'CEP válido'
-        //cep_teste.style.color = '#0c6800'
-
         document.querySelector('input[name=logradouro]').value = json.logradouro
         document.querySelector('input[name=bairro]').value = json.bairro
         document.querySelector('input[name=cidadeestado]').value = json.localidade
         document.querySelector('select[name=estadocidade]').value = json.uf
-    }; /*else {
+    } else {
         form.classList.add('was-validated');
-    }*/
+    };
 };
