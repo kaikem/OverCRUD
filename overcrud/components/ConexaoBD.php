bec52a144948758b48005206e344be3c110085f3
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-12-06 11:40:50 -0300

    Update ConexaoBD.php
    Retirado servidor casa.

--- overcrud/components/ConexaoBD.php
+++ overcrud/components/ConexaoBD.php
@@ -11,24 +11,12 @@ class ConexaoBD
     //conexão com bd
     public static function conectarBD()
     {
-        /*$db_name = 'overcrud';
-        $db_host = 'localhost:3307';
-        $db_user = 'root';
-        $db_port = '3307';
-        //$db_password = 'admin123';
-        $db_password = '';
-        $pdo = new PDO("mysql:dbname=$db_name;host=$db_host", $db_user, $db_password);
-        return $pdo;*/
-        
-
-        //BD CASA
         $db_name = 'overcrud';
         $db_host = 'localhost';
         $db_user = 'root';
-        $db_port = '3306';
-        $db_password = '';        
+        $db_port = '3307';
+        $db_password = '';
         $pdo = new PDO("mysql:dbname=$db_name;host=$db_host:$db_port", $db_user, $db_password);
         return $pdo;
-        //
     }
 };
