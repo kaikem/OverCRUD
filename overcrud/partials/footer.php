91ec759113180ba8ebcc78327e9a000913f74879
Kaike A. Maróstica <kaike.marostica@gmail.com>
2024-12-10 17:48:32 -0300

    Update footer.php
    link do email

--- overcrud/partials/footer.php
+++ overcrud/partials/footer.php
@@ -6,7 +6,7 @@ function footer($voltar)
         <div class='row d-flex flex-column justify-content-center align-items-center text-center'>
             <hr>
             <img src='$voltar/img/logo_footer.png' alt='Overdrive Softwares e Consultoria' style='width: 18rem'>
-            <a href='mailto:contato@overdriveconsultoria.com.br' class='mt-2' id='emailfooter'>contato@overdriveconsultoria.com.br</a>
+            <p class='text-primary mt-2' id='emailfooter'>contato@overdriveconsultoria.com.br</p>
         </div>
     </footer>
     ";
